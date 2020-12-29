f97ed6f0053f2e7d599eb04b6c192553eea21f68
matteo zhou <magiczhou04@gmail.com>
2020-12-29 22:18:18 +0100

    navbaricon

--- script.js
+++
@@ -1,6 +0,0 @@
-const checkbtn = document.getElementsByClassName('checkbtn')[0]
-const a = document.getElementsByClassName("a")[0]
-
-checkbtn.addEventListener('click',() => {
-    a.classList.toggle("active")
-})
